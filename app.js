d6c9c735737ec7c402d258763c67260d117374cb
jeongtae Lee <sms3025@ajou.ac.kr>
2023-11-04 21:50:03 +0900

    delete models/goal and modify models.usertocrew.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const session = express('express-session');
 const ExpressError = require('./utils/ExpressError');
 const diaryRoutes = require('./routes/diaries');
 const crewRoutes = require('./routes/crews');
+const exp = require('constants');
+const dbUrl = 'mongodb://127.0.0.1:27017/health-crew';
+mongoose.connect(dbUrl);
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.use('/diary', diaryRoutes);
 app.use('/crews', crewRoutes);
