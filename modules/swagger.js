a418ca0999d107a53d72e55712cc049b7a05eca6
Dongkwon Kim <dafeg3@ajou.ac.kr>
2023-11-23 18:14:58 +0900

    update diary, homepage and ranking controllers, homepage route and swagger file

--- modules/swagger.js
+++ modules/swagger.js
@@ -3,16 +3,16 @@ const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
     swaggerDefinition: {
-
+        openapi: "3.0.3",
         info: {
             title: 'Test API',
-            version: '1.0.0',
+            version: '2.0.0',
             description: 'Test API with express',
         },
         host: 'localhost:3000',
         basePath: '/'
     },
-    apis: ['./routes/*.js']
+    apis: ['./routes/*.js', './components/schemas/*']
 };
 
 const specs = swaggerJsdoc(options);
